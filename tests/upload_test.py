d7afb3a4e84356a09a4c7aee4e7455a3551ebc4b
jc2476 <97968604+jc2476@users.noreply.github.com>
2022-04-27 13:09:21 -0400

    created csv test

--- tests/upload_test.py
+++ tests/upload_test.py
@@ -1,9 +1,17 @@
-"""This tests that music.csv is uploaded"""
-
+"""This will test the music CSV upload"""
 import os
 
-def song_upload_test(client):
-   """Testing song uploads"""
-   response = client.get('/uploads/music.csv')
-   assert response.status_code == 200
-   assert os.path.isfile('music.csv')
+root = os.path.dirname(os.path.abspath(__file__))
+csvdir = os.path.join(root, '../app/uploads')
+
+def test_request_songs(client):
+    """This tests the song page"""
+    response = client.get("/songs")
+    assert response.status_code == 200
+    assert b"Browse: Songs" in response.data
+
+def test_songs_upload(application):
+    """This tests that songs are added to the database"""
+    assert os.path.exists(csvdir) is True
+    filepath = os.path.join(root, '../app/uploads/music.csv')
+    assert os.path.exists(filepath) is True
